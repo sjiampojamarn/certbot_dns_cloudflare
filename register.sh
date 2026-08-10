c40679b01f5552a9029a9dfacb7dded516b5f261
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2026-08-09 20:41:58 -0700

    Create register.sh for certificate management
    This script initializes configuration files, retrieves certificate information, and manages certificate creation and renewal using Certbot in a continuous loop.

---
+++ register.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## init files 
+touch /conf/cloudflare.ini
+touch /conf/email.ini
+touch /conf/register.list
+
+chmod 700 /conf/cloudflare.ini
+
+## run forever
+while true ; do
+  date
+  certbot certificates
+  
+  for d in $(cat /conf/register.list | sed "s/,/ /g") ; do 
+    echo $d ;
+    echo "Q" | openssl s_client -servername $d -connect ${d}:443 2>/dev/null | openssl x509 -noout -dates ;
+  done
+
+  while IFS= read -r line ; do  
+    echo $line
+    set -x
+    certbot certonly \
+      --non-interactive \
+      --agree-tos \
+      --email $(cat /conf/email.ini) \
+      --dns-cloudflare \
+      --dns-cloudflare-credentials /conf/cloudflare.ini \
+      --dns-cloudflare-propagation-seconds 630 \
+      --expand \
+      -d "$line" 
+    set +x
+  done < "/conf/register.list"
+  
+  ## Post command to prepare pem for HAProxy.
+  ./combineFullPrivKeys.sh
+
+  ## Attempt to renew/create every 12h.
+  set -x
+  sleep 12h
+  set +x
+
+done
