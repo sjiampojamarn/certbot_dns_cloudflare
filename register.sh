4c5395a1c64cee0b70af92bfd494054adbfc6653
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2026-09-20 07:13:58 -0700

    Unify register.sh and register_force.sh with --force and --once flags

--- register.sh
+++ register.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-## init files 
+force=0
+once=0
+
+for arg in "$@" ; do
+  case "$arg" in
+    --force) force=1 ;;
+    --once)  once=1  ;;
+    *)       echo "Unknown option: $arg (supported: --force, --once)" >&2 ; exit 1 ;;
+  esac
+done
+
+## init files
 touch /conf/cloudflare.ini
 touch /conf/email.ini
 touch /conf/register.list
 
 chmod 700 /conf/cloudflare.ini
 
-## run forever
+## run forever (or once with --once)
 while true ; do
   date
   certbot certificates
-  
-  for d in $(cat /conf/register.list | sed "s/,/ /g") ; do 
+
+  for d in $(cat /conf/register.list | sed "s/,/ /g") ; do
     echo $d ;
     echo "Q" | openssl s_client -servername $d -connect ${d}:443 2>/dev/null | openssl x509 -noout -dates ;
   done
 
-  while IFS= read -r line ; do  
+  while IFS= read -r line ; do
     echo $line
     set -x
-    certbot certonly \
+    if [ "$force" -eq 1 ] ; then
+      certbot certonly --force-renewal
+    else
+      certbot certonly
+    fi \
       --non-interactive \
       --agree-tos \
       --email $(cat /conf/email.ini) \
@@ -28,12 +43,16 @@ while true ; do
       --dns-cloudflare-credentials /conf/cloudflare.ini \
       --dns-cloudflare-propagation-seconds 630 \
       --expand \
-      -d "$line" 
+      -d "$line"
     set +x
   done < "/conf/register.list"
-  
+
   ## Post command to prepare pem for HAProxy.
   ./combineFullPrivKeys.sh
+
+  if [ "$once" -eq 1 ] ; then
+    break
+  fi
 
   ## Attempt to renew/create every 12h.
   set -x
